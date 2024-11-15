1a85bfbd08a546f40f2df4fc9294f2107e2ce3e2
christianTanaka251299 <christian.tanaka2512@gmail.com>
2024-11-15 14:38:44 +0700

    third commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 // Variabel yang Anda sediakan
 var client_id = "BRN-0210-1730515688330";
 var request_id = "cc682442-6c22-493e-8121-b9ef6b3fa728";
-var request_timestamp = "2024-11-15T06:30:36Z";
+var request_timestamp = "2024-11-15T07:34:00Z";
 var request_target = "/checkout/v1/payment"; // tanpa domain, hanya path
 var secret_key = "SK-9IT2zBMCQoYaa6Sch8B0";
 
@@ -62,17 +62,11 @@ function generateSignature(
 console.log("----- Digest -----");
 const jsonBody = JSON.stringify({
   order: {
-    invoice_number: "INV-20210124-0001",
-    amount: 150000,
+    amount: 20000,
+    invoice_number: "INV-20210231-0001",
   },
-  virtual_account_info: {
-    expired_time: 60,
-    reusable_status: false,
-    info1: "Merchant Demo Store",
-  },
-  customer: {
-    name: "Taufik Ismail",
-    email: "taufik@example.com",
+  payment: {
+    payment_due_date: 60,
   },
 });
 
